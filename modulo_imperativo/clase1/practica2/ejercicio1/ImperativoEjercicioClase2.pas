5ee642294fc7119cfb724f60c4b3ac5d88a6714e
nicoVillagran <nicolasvillagran321@gmail.com>
2026-08-28 21:26:56 -0300

    practica 2: ej1, modulo 'imprimirRecursivo' completado y funcionando.

--- modulo_imperativo/clase1/practica2/ejercicio1/ImperativoEjercicioClase2.pas
+++ modulo_imperativo/clase1/practica2/ejercicio1/ImperativoEjercicioClase2.pas
@@ -1,10 +1,9 @@
 {
   1.- Implementar un programa que invoque a los siguientes módulos.
     a. Un módulo recursivo que retorne un vector de a lo sumo 15 números enteros "random" mayores a 130 y menores a 145 (incluidos ambos).
-    La carga finaliza con 
-    el valor 130. 
-    b. Un módulo no recursivo que reciba el vector generado en a) e imprima el contenido del vector.
-    c. Un módulo recursivo que reciba el vector generado en a) e imprima el contenido del vector.
+    La carga finaliza con el valor 130. ✅
+    b. Un módulo no recursivo que reciba el vector generado en a) e imprima el contenido del vector. ✅
+    c. Un módulo recursivo que reciba el vector generado en a) e imprima el contenido del vector. ✅
     d. Un módulo recursivo que reciba el vector generado en a) y devuelva la suma de los valores contenidos en el vector.
     e. Un módulo recursivo que reciba el vector generado en a) y devuelva el máximo valor del vector.
     f. Un módulo recursivo que reciba el vector generado en a) y un valor y devuelva verdadero si dicho valor se encuentra en el vector o falso en caso contrario.
@@ -58,8 +57,20 @@ begin
 end;
 
 procedure ImprimirVectorRecursivo (v: vector; dimL: integer);
-begin    
-  {-- Completar --}     
+  procedure ImprimirRecursivo (v:vector; pos, dimL: integer);
+  begin
+    if (pos <= dimL) then begin
+      writeln('Elemento ', pos, ': ', v[pos]);
+      ImprimirRecursivo(v, pos+1, dimL);
+    end;
+  end;
+var pos: integer;
+begin
+  pos := 1;
+  if (dimL = 0) then
+    writeln('No hay elementos.')
+  else
+    ImprimirRecursivo(v, pos, dimL);
 end; 
     
 function Sumar (v: vector; dimL: integer): integer; 
@@ -102,8 +113,8 @@ begin
   writeln;
   if (dimL = 0) then writeln ('--- Vector sin elementos ---')
     else begin
-      ImprimirVector (v, dimL);
-      {  ImprimirVectorRecursivo (v, dimL);}
+      // ImprimirVector (v, dimL);
+      ImprimirVectorRecursivo (v, dimL);
     end;
   writeln;
   writeln;                   

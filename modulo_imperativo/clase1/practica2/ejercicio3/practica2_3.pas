9c447517a722741202b7c4d48e3831eec6581c81
nicoVillagran <nicolasvillagran321@gmail.com>
2026-08-31 17:19:25 -0300

    practica 2: ejercicio3 terminado.

--- modulo_imperativo/clase1/practica2/ejercicio3/practica2_3.pas
+++ modulo_imperativo/clase1/practica2/ejercicio3/practica2_3.pas
@@ -1,7 +1,9 @@
 {
   Escribir un programa que invoque a los siguientes módulos e informe el resultado:
     a. Un módulo recursivo que retorne un vector de a lo sumo 20 caracteres que conformen una 
-    palabra. La lectura de los caracteres termina en ‘.’ ✅
+      palabra. La lectura de los caracteres termina en ‘.’ ✅
+    b. Un módulo recursivo que reciba la “palabra” generada en a) y determine si dicha palabra es un palíndromo, es decir, si puede leerse
+      de la misma manera de izquierda a derecha que de derecha a izquierda.  Este módulo debe retornar el valor booleano correspondiente. ✅
 }
 
 program practica2_3;
@@ -31,14 +33,47 @@ procedure cargarVector(var v: vLetras;var dimL:integer);
       end;
   end;
 
+function esPalindromo(v: vLetras; dimL:integer):boolean;
+  function armarPalabra(v:vLetras; dimL, pos:integer):string;
+  begin
+    if (pos <= dimL) then armarPalabra := v[pos] + armarPalabra(v,dimL,pos+1)
+    else armarPalabra := '';
+  end;
+
+  function armarPalabraInv(v:vLetras; dimL, pos:integer):string;
+  begin
+    if (pos <= dimL) then armarPalabraInv := armarPalabraInv(v,dimL,pos+1) + v[pos]
+    else armarPalabraInv := '';
+  end;
+  
+  var 
+    pOrdenada, pInvertida: string;
+    pos:integer;
+  begin
+    if (dimL = 0) then esPalindromo := false
+    else
+      begin
+        pos := 1;
+        // ver si es palindromo
+        pOrdenada := armarPalabra(v, dimL, pos);
+        pInvertida := armarPalabraInv(v, dimL, pos);
+        
+        if (pOrdenada = pInvertida) then esPalindromo := true
+        else esPalindromo := false;
+      end;
+  end;
 // Var programa principal
 Var
   letras: vLetras;
   dimL: integer;
+  res:boolean;
 begin
   dimL := 0;
   //probar codigo...
   cargarVector(letras,dimL);
 
-  
+  res := esPalindromo(letras, dimL);
+
+  if res then writeln('la palabra es un palindromo')
+  else writeln('No es un palindromo');
 end.

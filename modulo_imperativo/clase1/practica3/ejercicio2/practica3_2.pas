41295eef9eb295f3f31b742af89c96ae62e1b885
nicoVillagran <95201246+nicoVillagran@users.noreply.github.com>
2026-09-01 17:29:09 -0300

    Add files via upload
    practica 3 ejercicio2

--- modulo_imperativo/clase1/practica3/ejercicio2/practica3_2.pas
+++ modulo_imperativo/clase1/practica3/ejercicio2/practica3_2.pas
@@ -5,14 +5,7 @@
       Finalizar con el código de producto 0. Un producto puede estar en más de una venta. (hay repetidos en el arbol)
       Se pide: 
         i. Generar y retornar un árbol binario de búsqueda de ventas ordenado por código de producto. Los códigos repetidos van a la derecha. ✅
-        ii. Generar y retornar otro árbol binario de búsqueda de productos vendidos ordenado por código de producto. Cada nodo del árbol debe contener 
-          el código de producto y la cantidad total de unidades vendidas.
-          * 
-          * - recorrer arbol 1
-          * - leer los datos, y generar el nodo de tipo "tProducto"
-          * - agregar al arbol --> modificar el modulo "agrgarHoja" para que si el codigo de producto se repite (es igual a..) se sume la cantidad vendida al campo "totalVentas" de ese nodo.
-          * 		---> cuando agregamos tenemos que contemplar si vamos a agregar o a sumar.
-          * 
+        ii. Generar y retornar otro árbol binario de búsqueda de productos vendidos ordenado por código de producto. Cada nodo del árbol debe contener ✅
         iii. Generar y retornar otro árbol binario de búsqueda de productos vendidos ordenado por código de producto. Cada nodo del árbol debe contener 
           el código de producto y la lista de las ventas realizadas del producto. Nota: No repetir información!!! 
           Nota: El módulo debe retornar TRES árboles.
@@ -42,6 +35,10 @@ type
     codProd: rango1;
     totalVentas: integer;
   end;
+  tVentaInfo2 = record
+	fecha: tFecha;
+	cantVentas: integer;
+  end;
   arbol = ^nodo;
   nodo = record
     dato: tVenta;
@@ -54,23 +51,34 @@ type
     HI: arbol2;
     HD: arbol2;
   end;
+  lVenta = ^nVenta;
+  nVenta = record
+   dato: tVenta;
+   sig: lVenta;
+  end;
+  arbol3 = ^nodoLista;
+  nodoLista = record
+    dato: lVenta;
+    HI: arbol2;
+    HD: arbol2;
+  end;
   
 // módulos
 procedure crearFecha(var f: tFecha);
   begin
     f.dia := random((30 - 1) + 1) + 1;
-    writeln('día: ', f.dia);
+    writeln('dia: ', f.dia);
     f.mes := random((12 - 1) + 1) + 1;
     writeln('mes: ', f.mes);
     f.anio := 2026;
-    writeln('año: ', f.anio);
+    writeln('anio: ', f.anio);
   end;
 procedure crearVenta(var v: tVenta);
   begin
     v.codProd := random(101);
     if(v.codProd <> 0)then
       begin
-        writeln('código: ', v.codProd);
+        writeln('codigo: ', v.codProd);
         crearFecha(v.fecha);
         v.cantVentas := random((10-1) + 1) + 1;
         writeln('cantidad de ventas: ', v.cantVentas);
@@ -116,24 +124,63 @@ procedure insertarOAcumular(var a: arbol2;elem:tProducto);
         if (elem.codProd < a^.dato.codProd) then insertarOAcumular(a^.HI, elem)
         else insertarOAcumular(a^.HD, elem);
   end;
-procedure reccorrerArbol(a: arbol; var a2: arbol2);
+procedure recorrerArbol(a: arbol; var a2: arbol2);
   var producto: tProducto;
   begin
     if(a <> nil)then
       begin
-        reccorrerArbol(a^.HI, a2);
+        recorrerArbol(a^.HI, a2);
         // acciones
         producto.codProd := a^.dato.codProd;
         producto.totalVentas := a^.dato.cantVentas;
-
         insertarOAcumular(a2, producto);
-        reccorrerArbol(a^.HD, a2);
+        
+        recorrerArbol(a^.HD, a2);
       end;
   end;
+
+procedure recorrerArbol3(a: arbol; var a3: arbol2);
+  var listaVentas: lVentas;
+  begin
+    if(a <> nil)then
+      begin
+        recorrerArbol(a^.HI, a2);
+        // acciones
+        // crear nodo
+        listaVentas^.dato
+        
+        recorrerArbol(a^.HD, a2);
+      end;
+  end;
+//--------------------------------
+procedure enOrden(a:arbol2);
+ begin
+  if (a <> nil)then
+  begin
+   enOrden(a^.HI);
+   writeln('producto: ',a^.dato.codProd);
+   writeln('cantidad de ventas totales: ',a^.dato.totalVentas);
+   enOrden(a^.HD);
+  end;
+ end;
+
+procedure modulo1();
+ var
+  a: arbol;
+  a2: arbol2;
+ begin
+  a:=nil;
+  a2:=nil;
+  // cargar primer arbol
+  cargarArbol1(a);
   
+  recorrerArbol(a, a2);
+  //enOrden(a2);
+ end;
 Begin
 	Randomize;
 	// generar y retornar ABB.
+	modulo1();
 End.
  
 
